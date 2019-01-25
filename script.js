fabf549d07d98a84628346ccbcee0c5cb8aa2356
Zeijls <45422060+Zeijls@users.noreply.github.com>
2019-01-25 14:36:52 +0100

    25 januari 2019 14.35

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 var button = document.querySelector('header > button');
-console.log("button",button);
+
 var form = document.querySelector('header > div');
-console.log("form",form);
 button.addEventListener('click', function() {
-  //console.log("haaaaiiii")
   form.classList.toggle('block');
 });
 
+var downloadButton = document.querySelector(".download");
 
-var buttonScrollDown = document.querySelector ('.scroll');
-console.log("buttonScrollDown fgrtgrt",buttonScrollDown)
+downloadButton.addEventListener("click", function(){
+  downloadButton.classList.add('start-download');
+  console.log(downloadButton);
+})
+
+var buttonScrollDown = document.querySelector('.scroll');
 
 buttonScrollDown.addEventListener('click', function(){
   var windowScroll = document.documentElement.clientHeight;
-  console.log(windowScroll);
+
     (function scroll(){
-      console.log(window.pageYOffset)
       if (window.pageYOffset < windowScroll) {
         window.scrollBy({
           top: 5000,
           behavior: 'smooth'
         });
       }
-      // if (window.pageYOffset > windowScroll) {
-      //   window.scrollTo(0, windowScroll);
-      // }
     })();
 });
